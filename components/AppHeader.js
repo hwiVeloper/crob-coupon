aaff12bc95fda1425e545ec62b37589c7c21e029
hwiVeloper <hwiveloper@gmail.com>
2020-07-02 10:08:01 +0900

    meta 추가, GA 추가

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -5,8 +5,35 @@ export default function AppHeader() {
   return (
     <>
       <Head>
-        <title>쿠키런 쿠폰 입력</title>
+        <title>쿠오븐 쿠폰 대량 입력</title>
         <link rel="icon" href="/favicon.ico" />
+
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+        <meta name="theme-color" content="#000000" />
+        
+        <meta name="author" content="hwiveloper" />
+        <meta name="description" content="쿠오븐 쿠폰 대량 입력 페이지입니다." />
+        <meta
+          name="keywords"
+          content="쿠키런,쿠오븐,쿠키런오븐브레이크,쿠키런쿠폰,쿠폰"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="쿠오븐 쿠폰 대량 입력" />
+        <meta property="og:description" content="쿠오븐 쿠폰 대량 입력 페이지입니다." />
+        {/* <meta property="og:image" content="" /> */}
+
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-99293532-4"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-99293532-4');
+        </script>
       </Head>
       <AppBar position="fixed">
         <Toolbar>
